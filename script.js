1ed181a23f7e6a9f853862c886332751f955258d
saroj <sarojaryal2003@gmail.com>
2024-03-08 10:01:32 +0545

    third commit

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 let data=4
 console.log(data);
+console.log(data);
+console.log(data);
+console.log(data);
+console.log(data);
+console.log(data);
+console.log(data);
+console.log(data);
+console.log(data);
